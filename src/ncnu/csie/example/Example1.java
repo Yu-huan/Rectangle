e2e7d307ec587e70ffa2bd1f8271908d7d0c0905
Yu-huan <sylvia8310@gmail.com>
2016-07-25 10:54:14 +0800

    Add files via upload

---
+++ src/ncnu/csie/example/Example1.java
@@ -0,0 +1,11 @@
+package ncnu.csie.example;
+
+public class Example1 {
+	public static void main(String argv[]) {
+		Rectangle r1=new Rectangle();
+		r1.w=10;
+		r1.h=5;
+		System.out.println("r1ªº­±¿n: "+r1.area());
+	}
+}
+
